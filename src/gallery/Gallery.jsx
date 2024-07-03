093bd80be78f24f7270bf75215308ccd49aae080
pya-y <p4ya.gcc@gmail.com>
2024-07-03 07:04:49 +0330

    fixed swiper css styles not loading

--- src/gallery/Gallery.jsx
+++ src/gallery/Gallery.jsx
@@ -1,42 +1,55 @@
 import React from "react";
 import { Pagination } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react/swiper-react";
-import "swiper/swiper.min.css";
-import "swiper/modules/pagination/pagination.min.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "./swiper.min.css";
+import "./pagination.min.css";
 import { Image } from "react-bootstrap";
 import "./gallery.css";
+
 const Gallery = ({ children }) => {
+    // const children = Array(5).fill(TestImage);
 
     return (
         <>
             <Swiper
                 slidesPerView={1}
                 spaceBetween={5}
-                direction="horizontal"
                 centeredSlides={true}
+                breakpoints={{
+                    // when window width is >= 320px
+                    320: {
+                        slidesPerView: 1,
+                        spaceBetween: 15,
+                    },
+                    // when window width is >= 480px
+                    480: {
+                        slidesPerView: 1,
+                        spaceBetween: 15,
+                    },
+
+                    // when window width is >= 640px
+                    640: {
+                        slidesPerView: 1.15,
+                        spaceBetween: 15,
+                    },
+                    800: {
+                        slidesPerView: 1.25,
+                        spaceBetween: 15,
+                    },
+                    960: {
+                        slidesPerView: 1.75,
+                        spaceBetween: 15,
+                    },
+                }}
                 pagination={{
                     clickable: true,
                 }}
                 modules={[Pagination]}
                 className="mySwiper"
-                navigation={{
-                    nextEl: ".swiper-button-next",
-                    prevEl: ".swiper-button-prev",
-                }}
             >
                 {children.map((x) => (
-                    <SwiperSlide className="bg-transparent">
-                        <div style={{ borderRadius: "1rem" }}>
-                            <Image
-                                className="gallery-item"
-                                fluid
-                                src={x}
-                                style={{
-                                    maxWidth: "70rem",
-                                    maxHeight: "40rem",
-                                }}
-                            />
-                        </div>
+                    <SwiperSlide style={{ height: "100%", width: "100%" }}>
+                        <Image className="gallery-item" fluid src={x} />
                     </SwiperSlide>
                 ))}
             </Swiper>
